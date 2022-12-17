486aec5ed19720c245716e7e9fcbf94d456a0253
Pham Thao Vy <ptvy0812@gmail.com>
2022-12-17 21:22:52 +0700

    update page fe page login, signup, 404 but all it can't display img

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 import cookieParser from 'cookie-parser';
 import rout from './routes/index.js';
-const app = express()
+
+import path from 'path'; // thu vien path de chuyen duong dan thu muc
+import { fileURLToPath } from 'url';
+//const morgan=require("morgan");
+//const handlebars= require("express-handlebars");
+const app = express();
 const port = 3000;
+const __filename=fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename);
 
 
 app.use(express.urlencoded({
@@ -11,16 +18,48 @@ app.use(express.urlencoded({
 }))
 
 app.use(cookieParser())
+
+// khai bao engine voi ten hbs
 app.engine('hbs', engine({
     // defaultLayout: 'main.hbs'
     extname: 'hbs',
     defaultLayout: 'main'
 }));
 
+app.use(express.static("public/img"));
 app.set('view engine', 'hbs');
 app.set('views', './views');
+//app.set('views', path.join(__dirname,'views/layouts/Teacher'));
+
+//console.log('Path:',path.join(__dirname,'views/Teacher'));
 app.use(rout); // router root 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+
+
+
+app.get("/",(req,res)=>{
+    return res.send("haha đây là dòng test");
+   
+  })
+  
+///// page 404
+app.get("/404",(req,res)=>{
+    // return res.send("haha đây là dòng test");
+    res.render("layouts/Error/404");
 })
-// Run :npm start 
+/// Page LogIn
+app.get("/login",(req,res)=>{
+    // return res.send("haha đây là dòng test");
+    res.render("layouts/Login/login");
+  })
+// Page SignUp
+app.get("/signup",(req,res)=>{
+    // return res.send("haha đây là dòng test");
+    res.render("layouts/Signup/signup");
+  })
+
+
+app.listen(port, () => {
+    console.log( `Example app listening at http://localhost:${port}`)
+})
+// Run :npm start
